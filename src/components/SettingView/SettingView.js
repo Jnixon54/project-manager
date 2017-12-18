59cb148c4ee6977edd27c5540981ce2bb133c6fe
Kelly Pham <Kellylpham@gmail.com>
2017-12-18 12:33:59 -0600

    setuprouter: router

---
+++ src/components/SettingView/SettingView.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class SettingView extends Component {
+  constructor() {
+    super((this.state = {}));
+  }
+  render() {
+    return <div />;
+  }
+}
