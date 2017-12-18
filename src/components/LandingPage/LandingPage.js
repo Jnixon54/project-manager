058cc81fa2755813a95561b8cd7e8eb911c10805
Bryce Hull <bahull@live.com>
2017-12-18 16:47:35 -0600

    Refactor: several changes to redux

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { setEmail, setPassword } from './../../ducks/reducers/userReducer';
-import axios from 'axios';
 
 class LandingPage extends Component {
   constructor(props) {
     super(props);
   }
 
-  submitLogin = () => {
-    // Waiting on backend
-    // axios.post('/api/login', {
-    //   email: this.props.email,
-    //   password: this.props.password
-    // });
-  };
-
   render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Email"
-          onChange={e => this.props.setEmail(e)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={e => this.props.setPassword(e)}
-        />
-
-        <button onClick={() => this.submitLogin()}>Test</button>
-      </div>
-    );
+    return <div>Landing Page</div>;
   }
 }
 const mapStateToProps = state => state.user;
 
-export default connect(mapStateToProps, { setEmail, setPassword })(LandingPage);
+export default connect(mapStateToProps, {})(LandingPage);
--- src/ducks/reducers/userReducer.js
+++ src/ducks/reducers/userReducer.js
@@ -1,23 +1,14 @@
 // State Variables
-const initialState = {
-  email: '',
-  password: ''
-};
+const initialState = {};
 
 // Action type
 const TEST_CASE = 'TEST_CASE';
-const UPDATE_EMAIL = 'UPDATE_EMAIL';
-const UPDATE_PASSWORD = 'UPDATE_PASSWORD';
 
 // Reducer
 function reducer(state = initialState, action) {
   switch (action.type) {
     case TEST_CASE:
       return { ...state, socketID: action.socketID };
-    case UPDATE_EMAIL:
-      return { ...state, email: action.payload };
-    case UPDATE_PASSWORD:
-      return { ...state, password: action.payload };
     default:
       return state;
   }
@@ -28,18 +19,6 @@ export function setUserID(userID) {
   return {
     type: 'TEST_CASE',
     userID
-  };
-}
-export function setEmail(e) {
-  return {
-    type: UPDATE_EMAIL,
-    payload: e.target.value
-  };
-}
-export function setPassword(e) {
-  return {
-    type: UPDATE_PASSWORD,
-    payload: e.target.value
   };
 }
 
