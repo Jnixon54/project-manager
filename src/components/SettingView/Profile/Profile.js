922b6592292dfbe3d7b4ef2ab59f5d860817e391
Kelly Pham <Kellylpham@gmail.com>
2017-12-19 16:46:53 -0600

    settingView: basic setting of all the path

---
+++ src/components/SettingView/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../SettingView.css';
+
+// import Header from '../../Header/Header';
+
+// import { Link } from 'react-router-dom';
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <br />
+        <div className="profile">
+          <h3>Look at my profile</h3>
+          {/* <Header /> */}
+        </div>
+      </div>
+    );
+  }
+}
