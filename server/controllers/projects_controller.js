b71d85ff3aa34ac0f7eeadde26df544ddb14d6aa
Bryce Hull <bahull@live.com>
2017-12-19 09:55:43 -0600

    Setup: server endpoints and controller for dashboard, Removed unneccessary server file

---
+++ server/controllers/projects_controller.js
@@ -0,0 +1,16 @@
+module.exports = {
+  getAllProjects: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    dbInstance
+      .getProjectById([req.body.id])
+      .then(response => res.status(200).send(response))
+      .catch(err => console.log(err));
+  },
+  getAllTasks: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    dbInstance
+      .getAllTasksById([req.body.id])
+      .then(response => status(200).send(response))
+      .catch(err => console.log(err));
+  }
+};
