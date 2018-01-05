598826c94893bf2cd7fe60c4df32f9daeea84930
Jnixon54 <Jnixon54@gmail.com>
2018-01-05 12:19:07 -0600

    Style: dashboard

--- src/components/SettingView/Settings/fire.js
+++ src/components/SettingView/Settings/fire.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase';
 
-var config = {
-  apiKey: 'AIzaSyDG0mwYNB1ITSelSm8rk0uEEMeC35pd1f4',
-  authDomain: 'groupii-c7929.firebaseapp.com',
-  databaseURL: 'https://groupii-c7929.firebaseio.com',
-  projectId: 'groupii-c7929',
-  storageBucket: 'groupii-c7929.appspot.com',
-  messagingSenderId: '507813912897'
-};
-
 const fire = firebase.initializeApp(config);
 
 export { fire };
